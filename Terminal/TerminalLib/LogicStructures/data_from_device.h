05fe181f973f4bd5dabda67afb5589d37cc617a6
Vitek <v_d_1984@hotmail.com>
2015-03-17 20:38:15 +0300

    Добавлены структуры - обертки команд от контроллера и компьютера

---
+++ Terminal/TerminalLib/LogicStructures/data_from_device.h
@@ -0,0 +1,154 @@
+#pragma once
+#include "stdafx.h"
+#include "device_protocol.h"
+
+/*!
+ * \file data_from_device.h
+ *
+ * \author Victor Mulika
+ * \date 17 Март 2015
+ *
+ * Структуры данных от устройства для работы в логике терминала
+ */
+
+
+namespace logic_structures
+{
+
+// базовая структура
+struct tag_base_data_from_device
+{
+protected:
+
+	const device_exchange::e_command_from_device command_id;	// id команды
+
+	tag_base_data_from_device(device_exchange::e_command_from_device cid) : command_id(cid)
+	{
+
+	}
+};
+
+// нажата кнопка (ОТСЧЁТ С 1)
+struct tag_button_press : tag_base_data_from_device
+{
+	byte button_number;	// номер нажатой кнопки
+
+	tag_button_press() 
+		: tag_base_data_from_device(device_exchange::e_command_from_device::button_press)
+		, button_number(0)
+	{
+		
+	}
+};
+
+// данные от купюроприёмника или монетоприёмника
+struct tag_bill_acceptor : tag_base_data_from_device
+{
+	uint16_t count;	// сумма
+
+	tag_bill_acceptor() : tag_base_data_from_device(device_exchange::e_command_from_device::bill_acceptor)
+		, count(0)
+	{
+
+	}
+};
+
+// Выдана монета хоппером
+struct tag_hopper_issue_coin : tag_base_data_from_device
+{
+	uint16_t balance;	// осталось выдать
+
+	tag_hopper_issue_coin() : tag_base_data_from_device(device_exchange::e_command_from_device::hopper_issue_coin)
+		, balance(0)
+	{
+
+	}
+};
+
+// выдана дисконтная карта
+struct tag_discount_card_issued : tag_base_data_from_device
+{
+	tag_discount_card_issued() : tag_base_data_from_device(device_exchange::e_command_from_device::discount_card_issued)
+	{
+
+	}
+};
+
+// выдана дисконтная карта
+struct tag_data_from_eeprom : tag_base_data_from_device
+{
+	byte cell_number;	// номер ячейки
+	uint32_t value;		// значение ячейки
+
+	tag_data_from_eeprom() : tag_base_data_from_device(device_exchange::e_command_from_device::data_from_eeprom)
+		, cell_number(0)
+		, value(0)
+	{
+
+	}
+};
+
+// состяние кнопок
+struct tag_buttons_state : tag_base_data_from_device
+{
+	bool button1;	// кнопка 1
+	bool button2;	// кнопка 2
+	bool button3;	// кнопка 3
+	bool button4;	// кнопка 4
+	bool button5;	// кнопка 5
+	bool button6;	// кнопка 6
+	bool button7;	// кнопка 7
+	bool button8;	// кнопка 8
+	bool button9;	// кнопка 9
+
+	tag_buttons_state() : tag_base_data_from_device(device_exchange::e_command_from_device::buttons_state)
+		,button1(false)
+		,button2(false)
+		,button3(false)
+		,button4(false)
+		,button5(false)
+		,button6(false)
+		,button7(false)
+		,button8(false)
+		,button9(false)
+	{
+
+	}
+};
+
+// подтверждение команды
+struct tag_command_confirmation : tag_base_data_from_device
+{
+	device_exchange::e_command_from_pc command;
+
+	tag_command_confirmation() : tag_base_data_from_device(device_exchange::e_command_from_device::command_confirmation)
+		, command(device_exchange::e_command_from_pc::empty)
+	{
+
+	}
+};
+
+// код ошибки устройства
+enum class e_device_error_code : byte
+{
+	error = 0x00,						// неизвестная ошибка
+	empty_hopper = 0x03,				// нет денег в хоппере, DATA0 = кол - во невыданных монет
+	discount_card_not_issued = 0x04,	// дисконтная карта не выдана
+	error_write_eeprom = 0x0B			// ошибка записи в EEPROM
+};
+
+// ошибка
+struct tag_error : tag_base_data_from_device
+{
+	e_device_error_code	code;	// код ошибки
+
+	tag_error() : tag_base_data_from_device(device_exchange::e_command_from_device::error)
+		, code(e_device_error_code::error)
+	{
+
+	}
+};
+
+
+
+}
---
+++ Terminal/TerminalLib/LogicStructures/data_from_pc.h
@@ -0,0 +1,110 @@
+#pragma once
+#include "stdafx.h"
+#include "device_protocol.h"
+
+/*!
+ * \file data_from_pc.h
+ *
+ * \author Victor Mulika
+ * \date 17 Март 2015
+ *
+ * Структуры данных от компьютера для работы в логике терминала
+ */
+
+
+namespace logic_structures
+{
+	// базовая структура
+	struct tag_base_data_from_pc
+	{
+	protected:
+
+		const device_exchange::e_command_from_pc command_id;	// id команды
+
+		tag_base_data_from_pc(device_exchange::e_command_from_pc cid) : command_id(cid)
+		{
+
+		}
+	};
+
+	// открыть клапан (ОТСЧЁТ С 1)
+	struct tag_open_valve : tag_base_data_from_pc
+	{
+		byte number;	// номер открываемого клапана
+
+		tag_open_valve() : tag_base_data_from_pc(device_exchange::e_command_from_pc::open_valve)
+			, number(0)
+		{
+
+		}
+	};
+
+	// закрыть клапан (ОТСЧЁТ С 1)
+	struct tag_close_valve : tag_base_data_from_pc
+	{
+		byte number;	// номер закрываемого клапана
+
+		tag_close_valve() : tag_base_data_from_pc(device_exchange::e_command_from_pc::close_valve)
+			, number(0)
+		{
+
+		}
+	};
+
+	// выдать монеты
+	struct tag_issue_coins : tag_base_data_from_pc
+	{
+		byte number;	// количество выдаваемых монет
+
+		tag_issue_coins() : tag_base_data_from_pc(device_exchange::e_command_from_pc::issue_coins)
+			, number(0)
+		{
+
+		}
+	};
+
+	// выдать дисконтную карту
+	struct tag_issue_discount_card : tag_base_data_from_pc
+	{
+		tag_issue_discount_card() : tag_base_data_from_pc(device_exchange::e_command_from_pc::issue_discount_card)
+		{
+
+		}
+	};
+
+	// прочитать данные eeprom
+	struct tag_read_eeprom : tag_base_data_from_pc
+	{
+		byte cell_number;	// номер ячейки
+
+		tag_read_eeprom() : tag_base_data_from_pc(device_exchange::e_command_from_pc::read_eeprom)
+			, cell_number(0)
+		{
+
+		}
+	};
+
+	// записать данные eeprom
+	struct tag_write_eeprom : tag_base_data_from_pc
+	{
+		byte		cell_number;	// номер ячейки
+		uint32_t	value;			// значение
+
+		tag_write_eeprom() : tag_base_data_from_pc(device_exchange::e_command_from_pc::write_eeprom)
+			, cell_number(0)
+			, value(0)
+		{
+
+		}
+	};
+
+	// запросить статус всех кнопок
+	struct tag_buttons_status : tag_base_data_from_pc
+	{
+		tag_buttons_status() : tag_base_data_from_pc(device_exchange::e_command_from_pc::buttons_status)
+		{
+
+		}
+	};
+
+}
