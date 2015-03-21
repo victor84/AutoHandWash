4d233b74690eef4a155b403aeca0999cfc46b387
Denis <schvedden@gmail.com>
2015-03-21 20:14:48 +0300

    Добавил шаблон страницы Users, на которой можно назначать группы пользователям

---
+++ Server/Modules/UserModule.cs
@@ -0,0 +1,18 @@
+﻿using Nancy.Security;
+
+namespace Server.Modules
+{
+    public class UserModule : BaseModule
+    {
+        public UserModule()
+        {
+            this.RequiresAuthentication();
+            Get["/users"] = Users;
+        }
+
+        private dynamic Users(dynamic parameters)
+        {
+            return View["Users", Model];
+        }
+    }
+}
